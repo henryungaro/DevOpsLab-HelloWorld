c55735161023f5aea726e5b2b6f02c708f4a1b4c
Vitor Prata <vitorbretasprata@gmail.com>
2021-09-28 00:49:00 -0300

    Correção das rotas

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("*")
-def index():
+@app.route("/<path:path>")
+def index(path):
     return render_template('404.html')
 
 if __name__ == '__main__':
